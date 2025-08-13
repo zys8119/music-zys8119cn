a9667327b547f79106fe18aabda391521c4ff01a
zys8119 <770959294@qq.com>
2025-08-13 17:26:08 +0800

    [add] deploy 78c09d2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,49 +1,49 @@
-import type { RouteRecordRaw } from 'vue-router'
+import type { RouteRecordRaw } from "vue-router";
 
 // 由于使用自动导入，需要显式导入这两个函数
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'home',
-    component: () => import('../views/HomeView.vue')
+    path: "/home",
+    name: "home",
+    component: () => import("../views/HomeView.vue"),
   },
   {
-    path: '/discover',
-    name: 'discover',
-    component: () => import('../views/DiscoverView.vue')
+    path: "/discover",
+    name: "discover",
+    component: () => import("../views/DiscoverView.vue"),
   },
   {
-    path: '/favorites',
-    name: 'favorites',
-    component: () => import('../views/FavoritesView.vue')
+    path: "/favorites",
+    name: "favorites",
+    component: () => import("../views/FavoritesView.vue"),
   },
   {
-    path: '/recent',
-    name: 'recent',
-    component: () => import('../views/RecentView.vue')
+    path: "/recent",
+    name: "recent",
+    component: () => import("../views/RecentView.vue"),
   },
   {
-    path: '/albums',
-    name: 'albums',
-    component: () => import('../views/AlbumsView.vue')
+    path: "/albums",
+    name: "albums",
+    component: () => import("../views/AlbumsView.vue"),
   },
   {
-    path: '/category/:id',
-    name: 'category',
-    component: () => import('../views/CategoryView.vue'),
-    props: true
-  }
-]
+    path: "/category/:id",
+    name: "category",
+    component: () => import("../views/CategoryView.vue"),
+    props: true,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
